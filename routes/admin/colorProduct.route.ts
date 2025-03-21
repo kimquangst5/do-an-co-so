8a6022132a880ecbe6e4df05c4404b0983263794
sonnguyen <nguyentrongngocson2412@gmail.com>
2025-03-21 21:45:02 +0700

    tính năng màu sản phẩm router

--- routes/admin/colorProduct.route.ts
+++ routes/admin/colorProduct.route.ts
@@ -5,7 +5,9 @@ import ROUTERS from "../../constants/routes/index.routes";
 const router = express.Router();
 const upload = multer();
 
-router.get(`${ROUTERS.ADMIN.COLOR_PRODUCT.INDEX}`, controller.index);
+router.get(`${ROUTERS.ADMIN.COLOR_PRODUCT.INDEX}`,
+
+    controller.index);
 router.patch(`${ROUTERS.ADMIN.COLOR_PRODUCT.UPDATE}`, controller.update);
 
 export default router;
